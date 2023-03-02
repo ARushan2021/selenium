8f0dcd6f438dcd1f022bb9fcd00f146cf831a9a8
ARushan@appline.ru <ARushan@appline.ru>
2023-03-02 16:35:06 +0300

    Шаг 1 в тесте написан

--- src/test/java/ibs_selenium/Lesson1.java
+++ src/test/java/ibs_selenium/Lesson1.java
@@ -1,4 +1,51 @@
 package ibs_selenium;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 
 public class Lesson1 {
+
+
+    @BeforeEach
+    public void before(){
+
+    }
+
+    @Test
+    public void test() {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://training.appline.ru/user/login");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        //Шаг.1 Авторизация
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//form[@id='login-form']"))));
+        driver.findElement(By.id("prependedInput")).sendKeys("Sekretar Kompanii");
+        driver.findElement(By.id("prependedInput2")).sendKeys("testing");
+        driver.findElement(By.id("_submit")).click();
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//h1[@class='oro-subtitle']"))));
+        //Шаг.2 Командировки
+
+
+        driver.quit();
+    }
+    @AfterEach
+    public void after(){
+
+
+    }
+
+
+
+
 }
+
