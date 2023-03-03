68d9baec950922b730ce8ed90f5245e7ff12717a
ARushan@appline.ru <ARushan@appline.ru>
2023-03-03 19:02:49 +0300

    Начал писать шаг 4

--- src/test/java/ibs_selenium/Lesson1.java
+++ src/test/java/ibs_selenium/Lesson1.java
@@ -3,12 +3,16 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.sql.Time;
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 
 public class Lesson1 {
@@ -16,7 +20,6 @@ public class Lesson1 {
 
     @BeforeEach
     public void before(){
-
     }
 
     @Test
@@ -28,24 +31,45 @@ public class Lesson1 {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         //Шаг.1 Авторизация
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//form[@id='login-form']"))));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(
+                By.xpath("//form[@id='login-form']"))));
         driver.findElement(By.id("prependedInput")).sendKeys("Sekretar Kompanii");
         driver.findElement(By.id("prependedInput2")).sendKeys("testing");
         driver.findElement(By.id("_submit")).click();
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//h1[@class='oro-subtitle']"))));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(
+                By.xpath("//h1[@class='oro-subtitle']"))));
         //Шаг.2 Командировки
+        driver.findElement(By.xpath("//*[@id='main-menu']/ul/li[2]/a/span[text()='Расходы']")).click();
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(
+                "//*[@id='main-menu']/ul/li[2]/a/span/ancestor::li//ul[@class='dropdown-menu menu_level_1']"))));
+        driver.findElement(By.xpath("//span[text()='Командировки']")).click();
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(
+                By.xpath("//div[@class='loader-mask shown']"))));
+        //Шаг. 3
+        driver.findElement(By.xpath("//div[1]/div/div/span/div[3]/div")).click();
+        driver.findElement(By.xpath("//div[1]/div/div/div[2]/div/div/input")).sendKeys("Согласование с ОСР");
+        driver.findElement(By.xpath("//label[@title='Согласование с ОСР']")).click();
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(
+                By.xpath("//div[@class='loader-mask shown']"))));
+        String id_number = driver.findElement(
+                By.xpath("//td[text()='Питер']//parent::tr//td[contains(@class, 'cell-name')]")).getText();
+        driver.findElement(
+                By.xpath("//div[1]/div/div/span/div[1]/div[1]")).click(); //  "//div[@class='filter-item oro-drop open-filter']/div[contains(text(), 'Номер')]"
+        wait.until(ExpectedConditions.visibilityOf(
+            driver.findElement(By.xpath("//input[@name='value']")))).sendKeys(id_number, Keys.ENTER);
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(
+                By.xpath("//div[@class='loader-mask shown']"))));
+        //Шаг.4 Переход в командтровку
+        driver.findElement(By.xpath("//tr/td[contains(@class, 'grid-cell')][text()='К21-10098']")).click();
 
 
-        driver.quit();
+
+
+
+        //driver.quit();
     }
     @AfterEach
     public void after(){
-
-
     }
-
-
-
-
 }
 
